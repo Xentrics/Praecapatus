013baa61c760481517a3b964d57e50cc0ebd0fcd
Bastian Seelbinder <Bastian Seelbinder>
2016-05-14 12:09:02 +0200

    minors

---
+++ Assets/Scripts/Exception/GameLogicException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Exception
+{
+    class GameLogicException : ApplicationException
+    {
+        public GameLogicException(string msg) : base(msg)
+        { }
+    }
+}
